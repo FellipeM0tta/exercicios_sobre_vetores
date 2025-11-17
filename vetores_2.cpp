7a62d428e5a93f69ebafac23ab357bc9b9032bc6
Fellipe Motta <arruda.fellipe@escola.pr.gov.br>
2025-11-17 14:18:47 -0300

    Create vetores_2.cpp

---
+++ vetores_2.cpp
@@ -0,0 +1,10 @@
+
+#include <iostream> 
+using namespace std; 
+#include <string>
+int main() 
+{ 
+   float preco[15] = {15.99, 10.10, 1.60, 2.65, 54.80, 38.98, 45.80, 47.90, 11.70, 14.40, 13.99, 30.65, 34.78, 12.76, 13.80}; 
+   cout << preco[13]; 
+    return 0;
+}
