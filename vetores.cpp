bd0e9a89631852f70b50f1dbe24a4fd0a71fc254
Fellipe Motta <arruda.fellipe@escola.pr.gov.br>
2025-11-17 14:18:21 -0300

    Create vetores.cpp

---
+++ vetores.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string frutas[15] = {"Maçã", "Banana", "Laranja", "Uva", "Abacaxi", "Manga", "Morango", "Melancia", "Limao", "Goiaba", "Maracujá", "Acerola", "Caju", "Kiwi", "Pera"};
+    
+    cout << frutas[11];
+
+    return 0;
+}
