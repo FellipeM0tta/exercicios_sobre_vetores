d518e95a043f0ea655623a06b4f8e5206e9df231
Fellipe Motta <arruda.fellipe@escola.pr.gov.br>
2025-11-17 14:19:16 -0300

    Create vetores_3.cpp

---
+++ vetores_3.cpp
@@ -0,0 +1,12 @@
+ #include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string marcas[15] = {"Toyota","Chevrolet","Ferrari","Mitsubishi","Volksvagen","Ford","Dodge"};
+    
+    cout << marcas[11];
+
+    return 0;
+}
