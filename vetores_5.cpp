03df04332565b6cade6262c3fad6080d44095fa5
Fellipe Motta <arruda.fellipe@escola.pr.gov.br>
2025-11-17 14:20:58 -0300

    Create vetores_5.cpp

---
+++ vetores_5.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string precos[7] = {"125000","95000","3100000","100000","30000","37000","98000"};
+    
+    cout << precos[6];
+
+    return 0;
+}
