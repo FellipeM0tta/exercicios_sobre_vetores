eb5b45a77957a713bc86a335d6e40bbf2b1989f9
Fellipe Motta <arruda.fellipe@escola.pr.gov.br>
2025-11-17 14:19:49 -0300

    Create vetores_4.cpp

---
+++ vetores_4.cpp
@@ -0,0 +1,9 @@
+#include <iostream> 
+using namespace std; 
+#include <string>
+int main() 
+{ 
+    string modelo[7] = {"Corolla", "Onix Plus", "Spider", "Novo ASX", "Virtus","F-1000","Dodge Dakota"}; 
+   cout << modelo[2]; 
+    return 0;
+}
